ab45e95c13c1f9d7765367c4832412c534405181
KennethLund <44909644+KennethLund@users.noreply.github.com>
2018-11-24 13:57:43 +0100

    Update TestBot.py

--- TestBot.py
+++ TestBot.py
@@ -195,6 +195,4 @@ async def weathertilst():
 
 token = 'NTA5Njc0NDQ5NzY3NzU4ODcy.Dsro3g.KoC7H7PSECN1PrmZKf3a5ytDV3U'
 pancakes = 'NTA5Njc0NDQ5NzY3NzU5ODcy.Dtrarw.ndFwxW_xv7PjSh9OU5IJUihMF6I'
-with = pancakes
-blueberries = with
-bot.run(blueberries)
+bot.run(pancakes)
